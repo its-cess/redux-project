6186bba6ef20078eaef45e3230442b5a346055f9
Cecily Toro <torocecily@gmail.com>
2022-09-20 11:18:56 -0400

    add: actions and reducers

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,13 +1,8 @@
+import { Action } from "@remix-run/router";
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-  products: [
-    {
-      id: 1,
-      title: "Cecily",
-      category: "programmer"
-    }
-  ]
+  products: []
 };
 
 // Reducers always take two parameters. (type, action). Here we have destructed the action to {type, payload} as we set it up in
@@ -15,7 +10,18 @@ const initialState = {
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
+      return { ...state, products: payload };
+    default:
       return state;
+  }
+};
+
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCT:
+      return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
     default:
       return state;
   }
